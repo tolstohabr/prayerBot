b7029b9dc2de4c9d9c462a9755e67184c13a4f85
Temirlan Ragimov <temirlan.official@mail.ru>
2026-03-14 21:24:48 +0300

    добавление в БД

--- go.mod
+++ go.mod
@@ -4,9 +4,13 @@ go 1.25.4
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.8.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mitchellh/hashstructure v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/tucnak/telebot v2.0.0+incompatible // indirect
+	golang.org/x/text v0.29.0 // indirect
 	gopkg.in/telebot.v3 v3.3.8 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 	tb "gopkg.in/telebot.v3"
 )
@@ -21,6 +23,24 @@ func main() {
 	if botToken == "" {
 		log.Fatal("BOT_TOKEN не установлен")
 	}
+
+	connStr := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatal("Ошибка подключения к базе:", err)
+	}
+
+	log.Println("Подключение к базе успешно")
+
+	defer conn.Close(context.Background())
 
 	bot, err := tb.NewBot(tb.Settings{
 		Token:  botToken,
@@ -55,6 +75,20 @@ func main() {
 
 		lat := loc.Lat
 		lon := loc.Lng
+
+		chatID := c.Sender().ID
+
+		_, err := conn.Exec(context.Background(),
+			`INSERT INTO users (chat_id, latitude, longitude, subscribed)
+		 VALUES ($1, $2, $3, FALSE)
+		 ON CONFLICT (chat_id) DO UPDATE
+		 SET latitude = EXCLUDED.latitude,
+		     longitude = EXCLUDED.longitude`,
+			chatID, lat, lon,
+		)
+		if err != nil {
+			log.Println("Ошибка при вставке в БД:", err)
+		}
 
 		msg := fmt.Sprintf(
 			"Результат\n\nШирота: %.6f\nДолгота: %.6f",
